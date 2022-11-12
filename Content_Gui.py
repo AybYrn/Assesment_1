c4b8f369271f100e250523655137f628f84c4753
AybYrn <aybukeyaren--@outlook.com>
2022-11-12 22:50:21 +0000

    Database has been structured

--- Content_Gui.py
+++ Content_Gui.py
@@ -12,48 +12,166 @@ class Content_Gui:
         connection = sqlite3.connect("./data/dishes.db")
         cursor = connection.cursor()
 
-        # Name of Dish, Who upload, Ingredient, Instructions, Number of rates, Avg rate, Type, photo
-        data = [
-            ("TChorizo & mozzarella gnocchi bake", "By Marianne Turner",
-             "1 tbsp olive oil - 1 onion, finely chopped - 2 garlic cloves, crushed - 120g chorizo, diced - 2 x 400g "
-             "cans chopped tomatoes - 1 tsp caster sugar - 600g fresh gnocchi - 125g mozzarella ball, cut into chunks "
-             "- small bunch of basil, torn - green salad, to serve",
-             "STEP 1 \nHeat the oil in a medium pan over a medium heat. Fry the onion and garlic for 8-10 mins until "
-             "soft. Add the chorizo and fry for 5 mins more. Tip in the tomatoes and sugar, and season. Bring to a "
-             "simmer, then add the gnocchi and cook for 8 mins, stirring often, until soft. Heat the grill to high. "
-             "\nSTEP 2\nStir ¾ of the mozzarella and most of the basil through the gnocchi. Divide the mixture "
-             "between six ovenproof ramekins, or put in one baking dish. Top with the remaining mozzarella, "
-             "then grill for 3 mins, or until the cheese is melted and golden. Season, scatter over the remaining "
-             "basil and serve with green salad.",
-             483, 5, "Main", "./assets/gnocchi.png"),
-            ("Easy chocolate fudge cake", "By Member recipe by misskay",
-             "150ml sunflower oil, plus extra for the tin - 175g self-raising flour - 2 tbsp cocoa  - 1 tsp bicarbonate"
-             "of soda - 150g caster sugar - 2 tbsp golden syrup - 2 large eggs, lightly beaten - 150ml semi-skimmed "
-             "milk - 100g unsalted butter - 225g icing sugar - 40g cocoa powder - 2½ tbsp milk",
-             "STEP 1\nHeat the oven to 180C/160C fan/gas 4. Oil and line the base of two 18cm sandwich tins. Sieve the"
-             "flour, cocoa powder and bicarbonate of soda into a bowl. Add the caster sugar and mix well.\nSTEP "
-             "2\nMake a well in the centre and add the golden syrup, eggs, sunflower oil and milk. Beat well with an"
-             " electric whisk until smooth.\nSTEP 3\nPour the mixture into the two tins and bake for 25-30 mins until "
-             "risen and firm to the touch. Remove from oven, leave to cool for 10 mins before turning out onto a"
-             " cooling rack.\nSTEP 4\nTo make the icing, beat the unsalted butter in a bowl until soft. Gradually "
-             "sieve and beat in the icing sugar and cocoa powder, then add enough of the milk to make the icing fluffy"
-             " and spreadable.\nSTEP 5\nSandwich the two cakes together with the butter icing and cover the sides and"
-             " the top of the cake with more icing.",
-             939, 5, "Dessert", ".assets/fudgecake.png"),
-            ("Pixie", "UK", 2021, 5, 3.1, "Comedy", True, "Drug Caper"),
-            ("Martian", "US", 2015, 5, 4.6, "SciFi", False, "Spacetrip to Mars"),
-            ("Grimbsy", "UK", 2016, 5, 3.6, "Comedy", True, "Action Comedy"),
-            ("We're the Millers", "US", 2013, 5, 4.1, "Comedy", False, "Road Movie"),
-            ("First Man", "US", 2018, 5, 4.1, "SciFi", True, "First Moon Landing"),
-            ("Brian and Charles", "UK", 2022, 5, 3.1, "Comedy", False, "AI Robot Inventor"), ]
+        try:
+            cursor.execute(
+                "CREATE TABLE "
+                "Dishes(NameOfDish, PostedBy, Ingredient, Instructions, numOfRates, AvgRate, Type, PhotoLocation)")
 
-    #def displayDishByName():
+            # Name of Dish, Who upload, Ingredient, Instructions, Number of rates, Avg rate, Type, photo
+            data = [
+                ("TChorizo & mozzarella gnocchi bake", "By Marianne Turner",
+                 "1 tbsp olive oil - 1 onion, finely chopped - 2 garlic cloves, crushed - 120g chorizo, diced - 2 x 400g "
+                 "cans chopped tomatoes - 1 tsp caster sugar - 600g fresh gnocchi - 125g mozzarella ball, cut into chunks "
+                 "- small bunch of basil, torn - green salad, to serve",
+                 "STEP 1 \nHeat the oil in a medium pan over a medium heat. Fry the onion and garlic for 8-10 mins until "
+                 "soft. Add the chorizo and fry for 5 mins more. Tip in the tomatoes and sugar, and season. Bring to a "
+                 "simmer, then add the gnocchi and cook for 8 mins, stirring often, until soft. Heat the grill to high. "
+                 "\nSTEP 2\nStir ¾ of the mozzarella and most of the basil through the gnocchi. Divide the mixture "
+                 "between six ovenproof ramekins, or put in one baking dish. Top with the remaining mozzarella, "
+                 "then grill for 3 mins, or until the cheese is melted and golden. Season, scatter over the remaining "
+                 "basil and serve with green salad.",
+                 483, 5, "Main", "./assets/gnocchi.png"),
+                ("Smoked salmon & lemon risotto", "By Good Food team",
+                 "1 onion, finely chopped - 2 tbsp olive oil - 350g risotto rice, such as Arborio - 1 garlic clove, finely"
+                 " chopped - 1 ½l boiling vegetable stock - 170g pack smoked salmon, three-quarters chopped - 85g "
+                 "mascarpone lite - 3 tbsp flat-leaf parsley, chopped - grated lemon zest, plus squeeze of juice - handful "
+                 "rocket",
+                 "STEP 1\nFry the onion in the oil for 5 mins. Add the rice and garlic, then cook for 2 mins, stirring"
+                 " continuously. Pour in a third of the stock and set the timer to 20 mins. Simmer, stirring occasionally,"
+                 " until the stock has been absorbed, then add half the rest of the stock and carry on cooking, stirring a"
+                 " bit more frequently, until that has been absorbed.\nSTEP 2\nPour in the last of the stock, stir, then"
+                 " simmer until cooked and creamy. Take from the heat and add the chopped salmon, mascarpone, parsley and"
+                 " lemon zest. Grind in some black pepper, but don’t add salt as the salmon will be salty enough. Leave for"
+                 " 5 mins to settle, then taste and add a little lemon juice if you like. Serve topped with reserved salmon"
+                 " (roughly torn) and some rocket.",
+                 269, 4, "Main", "./assets/salmonlemonrisotto.png"),
+                ("Smoked trout, beetroot & horseradish flatbread", "By Good Food team"
+                 "4 flatbreads - olive oil, for brushing - 2 tbsp creamed horseradish - 2 tbsp crème fraîche - small bunch"
+                 " dill, ½ chopped, ½ picked into small fronds - squeeze lemon juice, plus a pinch of zest - 3 cooked"
+                 " beetroot (not in vinegar), very thinly sliced - 4 smoked trout fillets, broken into large flakes - "
+                 "mixed salad leaves, to serve (optional)",
+                 "STEP 1\nHeat oven to 220C/200C fan/gas 7. Brush the flatbreads with olive oil. Put on a large baking"
+                 " sheet and pop in the oven for about 8 mins until crisp round the edges.\nSTEP 2\nMeanwhile, mix the "
+                 "horseradish, crème fraîche, chopped dill, lemon juice and zest, and some seasoning. Add a few drops of "
+                 "water to loosen mixture to a drizzling consistency.\nSTEP 3\nTop each flatbread with some beetroot slices"
+                 " and smoked trout. Drizzle over the horseradish sauce, sprinkle with dill fronds and serve with salad,"
+                 " if you like.",
+                 2, 1, "Main", "./assets/beetrootflatbread.png"),
 
-    #def displayAllDishes():
+                ("Easy chocolate fudge cake", "By Member recipe by misskay",
+                 "150ml sunflower oil, plus extra for the tin - 175g self-raising flour - 2 tbsp cocoa  - 1 tsp bicarbonate"
+                 "of soda - 150g caster sugar - 2 tbsp golden syrup - 2 large eggs, lightly beaten - 150ml semi-skimmed "
+                 "milk - 100g unsalted butter - 225g icing sugar - 40g cocoa powder - 2½ tbsp milk",
+                 "STEP 1\nHeat the oven to 180C/160C fan/gas 4. Oil and line the base of two 18cm sandwich tins. Sieve the"
+                 "flour, cocoa powder and bicarbonate of soda into a bowl. Add the caster sugar and mix well.\nSTEP "
+                 "2\nMake a well in the centre and add the golden syrup, eggs, sunflower oil and milk. Beat well with an"
+                 " electric whisk until smooth.\nSTEP 3\nPour the mixture into the two tins and bake for 25-30 mins until "
+                 "risen and firm to the touch. Remove from oven, leave to cool for 10 mins before turning out onto a"
+                 " cooling rack.\nSTEP 4\nTo make the icing, beat the unsalted butter in a bowl until soft. Gradually "
+                 "sieve and beat in the icing sugar and cocoa powder, then add enough of the milk to make the icing fluffy"
+                 " and spreadable.\nSTEP 5\nSandwich the two cakes together with the butter icing and cover the sides and"
+                 " the top of the cake with more icing.",
+                 939, 5, "Dessert", "./assets/fudgecake.png"),
+                ("Vegan millionaire's shortbread", "By Cassie Best",
+                 "For the base\n150g cashew nuts - 50g rolled oat - 4 medjool dates, pitted- 50g coconut oil, melted - "
+                 "\nFor the filling\n - 350g pitted medjool dates - 125ml unsweetened almond milk - 25ml maple syrup - "
+                 "150g coconut oil - 1 tsp vanilla extract - \nFor the topping\n - 150g coconut oil - 5 tbsp cocoa powder "
+                 "- 2 tsp maple syrup",
+                 "STEP 1\nGrease a 20cm square cake tin and line with baking parchment. Tip the cashew nuts and oats into"
+                 " a food processer and blitz to crumbs. Add the dates and coconut oil, and blend again. Transfer to the"
+                 " tin and use a spoon to press the nutty mixture into a compact, even layer that covers the base. Chill"
+                 " while you prepare the filling.\nSTEP 2\nFor the filling, add the dates, almond milk, maple syrup and "
+                 "coconut oil to a saucepan with a generous pinch of salt and bring to a simmer. Boil for 2-3 mins until "
+                 "the dates are really soft. Cool the caramel a little, then tip into the blender, add the vanilla extract "
+                 "and blitz to a smooth purée. The caramel will be hot so be careful when handling. Add a little more salt "
+                 "if the mixture is too sweet. Pour over the nutty base and spread to the sides of the tin, getting the "
+                 "surface as smooth as possible. Chill while you prepare the topping.\nSTEP 3\nGently heat the coconut oil "
+                 "in a saucepan until melted. Remove from the heat and whisk in the cocoa and maple syrup until there are"
+                 " no lumps. Cool for 10 mins, pour over the caramel layer and return to the fridge for at least 3 hrs or "
+                 "until firmly set. To serve, cut into squares.",
+                 36, 4, "Dessert", ".assets/veganshortbread.png"),
+                ("Ginger biscuits", "By Liberty Mendez",
+                 "100g salted butter, cubed - 75g light brown soft sugar - 1 tbsp grated fresh ginger - 100g golden syrup "
+                 "- 250g self-raising flour - 1½ tbsp ground ginger - 1 tsp bicarbonate of soda - 1 small egg yolk, beaten",
+                 "STEP 1\nHeat the oven to 190C/170C fan/gas 5. Line two large baking trays with baking parchment. In a "
+                 "saucepan over a low heat, melt the butter, sugar, fresh ginger and golden syrup and leave to cool.\n"
+                 "STEP 2\n Mix the flour, ground ginger and bicarbonate of soda together in a bowl with a wooden spoon. "
+                 "Gradually stir in the cooled sugar mixture and the egg yolk and knead briefly to make a dough.\nSTEP 3\n"
+                 "Roll the dough into 20g balls and put on the prepared baking trays with 3cm between each to allow for "
+                 "spreading. Bake for 8-10 mins until golden brown. Leave to cool on the trays for a min, then transfer "
+                 "to a cooling rack to cool completely.",
+                 161, 5, "Dessert", "./assets/gingerbiscuit.png"),
 
-    #def displayDishByType():
+                ("Super berry smoothie", "By Good Food team",
+                 "450g bag frozen berry - 450g pot fat-free strawberry yogurt - 100ml milk - 25g porridge oat - 2 tsp honey"
+                 , "Whizz the berries, yogurt and milk together with a stick blender until smooth. Stir through the "
+                   "porridge oats, then pour into 4 glasses and serve with a drizzle of honey, if you like.",
+                 20, 4, "Drink", "./assets/berrysmoothie.png"),
+                ("Peanut butter smoothie", "By Miriam Nice",
+                 "200ml oat milk - 1 banana, peeled and chopped - 20g peanut butter - 1 tbsp rolled oats - pinch of "
+                 "cinnamon - pinch of allspice - pinch of nutmeg (optional) - small handful ice cubes",
+                 "Whizz all the ingredients together in a blender (one suitable for crushing ice) until smooth. Pour into "
+                 "a tall glass and serve.",
+                 20, 3, "Drink", "./assets/peanutsmoothie.png"),
 
-    #def displayDishByRate():
+                ("Miso & butternut soup", "By Sara Buenfeld",
+                 "2 tsp rapeseed oil - 1 large onion, chopped - 400g butternut squash, skin-on, cut into chunks - 2 garlic"
+                 " cloves, chopped - 210g can butter beans, drained - 2 tsp vegetable bouillon - 80g shredded kale, finely"
+                 " chopped - 2 tsp sesame oil - 2 tsp toasted sesame seeds - 2 tsp finely grated ginger - 1 tbsp brown"
+                 " rice miso",
+                 "STEP 1\nHeat the oil in a large pan and fry the onion for 5 mins to soften. Add the squash and garlic,"
+                 " and stir for a minute. Add the beans and bouillon along with a litre of water, then cover and simmer "
+                 "for 20 mins until the squash is tender.\nSTEP 2\nMeanwhile, steam the kale for 10 mins, then toss"
+                 " together with the sesame oil, seeds and ginger.\nSTEP 3\nAdd the miso to the soup, then blitz until "
+                 "smooth using a hand blender. Pour into bowls and top with the sesame kale mix to serve.",
+                 87, 5, "Starter", "./assets/misosoup.png"),
+                ("Cauliflower soup", "By Esther Clark",
+                 "1 large cauliflower (1.5kg), cut into florets - ½ tbsp ground cumin - 2 tbsp olive oil, plus extra"
+                 " for drizzling - 4 thyme sprigs - 1 onion, finely chopped - 1 celery stick, finely chopped - 1 garlic"
+                 " clove, crushed - 750-850ml veg or chicken stock - 100ml single cream - ½ small bunch of parsley, "
+                 "finely chopped",
+                 "STEP 1\nHeat the oven to 220C/200C fan/gas 7. Toss the cauliflower florets in a roasting tin with 1 tbsp "
+                 "olive oil, the cumin and the thyme. Roast for 15 mins or until golden and tender. Discard the thyme.\n"
+                 "STEP 2\nHeat the remaining oil in a saucepan with the onion and celery and fry over a medium heat for 10"
+                 " mins or until softened. Add the garlic and cook for 1 min. Stir through most of the cauliflower,"
+                 " reserving some to top the soup with later. Add 750ml of the stock to the pan and bring to a simmer. Cook"
+                 " for 10 mins.\nSTEP 3\nBlitz the soup until smooth using a hand blender or food processor. Stir through"
+                 " the cream and season to taste. Add extra stock if you like your soup a little thinner. Ladle into bowls"
+                 " and top with the parsley, reserved cauliflower and an extra drizzle of olive oil.",
+                 3, 3, "Starter", "./assets/cauliflowersoup.png"),
+
+                ("Air fryer sweet potato fries", "By Samuel Goldsmith",
+                 "2 sweet potatoes - 1 tsp olive oil - For the seasoning - 1 tsp sweet smoked paprika or other seasonings"
+                 " such as Cajun seasoning, BBQ seasoning or garlic granules - ½ tsp mixed herbs.",
+                 "STEP 1\nPeel the sweet potatoes and slice into ½ cm sticks. Put in a bowl, drizzle over the oil and give"
+                 " them a good toss to coat. Sprinkle over the paprika, mixed herbs and ½ tsp each of salt and pepper, then"
+                 " give it all a good mix with your hands to ensure it's evenly coated.\nSTEP 2\nCook, in batches, in a"
+                 " single layer in your air fryer basket at 180C for around 10-15 mins, until lightly golden, giving a"
+                 " little shake or toss after 5 mins. Once all the batches have cooked, return the fries to the air fryer,"
+                 " give it a good shake and cook for a further 1-2 mins to heat through.",
+                 4, 2, "Side", "./assets/sweetpotatofries.png"),
+                ("Chestnut hummus", "By Barney Desmazery",
+                 "180g vacuum-packed cooked chestnuts - 1 lemon, juiced - 1 garlic clove - 4 tbsp olive oil - pitta crisps"
+                 " and vegetable crudités, to serve",
+                 "STEP 1\nTake one chestnut from the pack and set aside. Tip the rest of the chestnuts into a blender or"
+                 " smoothie maker with the lemon juice, garlic, oil and a little freshly boiled water from the kettle."
+                 " Season. Blitz to a smooth purée, then spoon into a bowl or spread over four plates.\nSTEP 2\nFinely "
+                 "grate over the reserved chestnut, then serve with pitta crisps and vegetable crudités.",
+                 3, 3, "Side", "./assets/chestnuthummus.png")
+                 ]
+            cursor.executemany("INSERT INTO Dishes VALUES(?, ?, ?, ?, ?, ?, ?, ?)", data)
+            connection.commit()
+        except:
+            print("Dishes Table already exists")
+
+
+    # def displayDishByName():
+
+    # def displayAllDishes():
+
+    # def displayDishByType():
+
+    # def displayDishByRate():
 
     def __init__(self, master):
         self.master = master
